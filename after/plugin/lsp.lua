f02bb697aa12704b95c1a2c839f0ce1a439813b7
Ivan Santiago Chauta Gaviria <santiagochauta@gmail.com>
2024-01-19 00:07:39 -0500

    change remap for some shortcuts

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -40,8 +40,8 @@ cmp.setup({
   },
   formatting = lsp_zero.cmp_format(),
   mapping = cmp.mapping.preset.insert({
-    ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<A-k>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<A-j>'] = cmp.mapping.select_next_item(cmp_select),
     ['<Enter>'] = cmp.mapping.confirm({ select = true }),
     ['<C-Space>'] = cmp.mapping.complete(),
   }),
